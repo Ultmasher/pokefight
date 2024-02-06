0822118e7e4dea6179be0b2db8d15af8c71c4441
Tania Boterman <taniaboterman@gmail.com>
2024-02-06 16:40:33 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+// import pokemon from "./routes/pokemonRouter.js";
+
+
+dotenv.config();
+const app = express();
+const port = 8000;
+
+app.use(cors());
+app.use(express.json());
+// app.use('/pokemon', pokemonRouter);
+
+//define route for req and / is the root path
+//then we fire handler function when get request
+//and send response to client
+app.get('/', (req, res) => {
+    res.send('Pokemon Fighter');
+});
+
+
+//start server by calling listen
+app.listen(port, () => {
+   console.log(`Recipes app is running at http://localhost:${port}`) 
+});
