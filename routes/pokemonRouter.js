410fb89e3edc768c0be390e4cbad6a9b357060bf
Moustafa Nady <mostafanadii93@gmail.com>
2024-02-08 17:30:39 +0100

    removing dublications

--- routes/pokemonRouter.js
+++ routes/pokemonRouter.js
@@ -6,8 +6,6 @@ const pokemonRouter = express.Router();
 pokemonRouter.get('/pokemon/:id/:info', infoPokemon);
 pokemonRouter.get('/:id', getPokemon_id);
 pokemonRouter.get('/', getPokemons);
-recipesRouter.get('/', getPokemons);
-recipesRouter.get('/:id', getPokemon_id);
-recipesRouter.get('/:id/:info', infoPokemon);
+
 
 export default pokemonRouter;
